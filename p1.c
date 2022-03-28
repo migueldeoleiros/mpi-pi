72686f6315d94ba2dd521bfec4cd290624f3fa68
migueldeoleiros <migueldeoleiros@gmail.com>
2022-03-28 11:16:31 +0200

    añadida la librería y arreglados los tabuladores

--- p1.c
+++ p1.c
@@ -1,15 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
+#include <mpi.h>
 
-int main(int argc, char *argv[])
-{
+int main(int argc, char *argv[]) {
     int i, done = 0, n, count;
     double PI25DT = 3.141592653589793238462643;
     double pi, x, y, z;
 
-    while (!done)
-    {
+    while (!done) {
         printf("Enter the number of points: (0 quits) \n");
         scanf("%d",&n);
     
@@ -19,14 +18,14 @@ int main(int argc, char *argv[])
 
         for (i = 1; i <= n; i++) {
             // Get the random numbers between 0 and 1
-	    x = ((double) rand()) / ((double) RAND_MAX);
-	    y = ((double) rand()) / ((double) RAND_MAX);
+            x = ((double) rand()) / ((double) RAND_MAX);
+            y = ((double) rand()) / ((double) RAND_MAX);
 
-	    // Calculate the square root of the squares
-	    z = sqrt((x*x)+(y*y));
+            // Calculate the square root of the squares
+            z = sqrt((x*x)+(y*y));
 
-	    // Check whether z is within the circle
-	    if(z <= 1.0)
+            // Check whether z is within the circle
+            if(z <= 1.0)
                 count++;
         }
         pi = ((double) count/(double) n)*4.0;
