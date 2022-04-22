cdfa0fc57c8f896657c9b24a5b6d03b988c2a698
xxoel <xoeldiaaaazsu@gmail.com>
2022-04-22 16:42:38 +0200

    MPI_BinomialCast working

--- pi.c
+++ pi.c
@@ -28,6 +28,24 @@ int MPI_FlattreeColectiva(void * buff, void *recvbuff, int count,
     return MPI_SUCCESS;
 }
 
+int MPI_BinomialCast(void *buff, int count, MPI_Datatype datatype, int root, MPI_Comm comm){
+    int rank,numprocs;
+    MPI_Comm_size(comm,&numprocs);
+    MPI_Comm_rank(comm,&rank);
+    MPI_Status status;
+    for(int i=1;i<numprocs-1;i++){
+        int umbral = pow(2,i-1);
+        if(rank<umbral)
+            MPI_Send(buff,count,datatype,rank+umbral,0,comm);
+        else{
+            if(rank<pow(2,i)){
+                MPI_Recv(buff,1,datatype,MPI_ANY_SOURCE,0,comm,&status);
+            }
+        }
+    }
+    return MPI_SUCCESS;
+}
+
 int main(int argc, char *argv[]) {
     int i, done = 0, n, count, localCount;
     int numprocs,rank;
@@ -44,13 +62,13 @@ int main(int argc, char *argv[]) {
             printf("Enter the number of points: (0 quits) \n");
             scanf("%d",&n);
         }
-        MPI_Bcast(&n,1,MPI_INT,0,MPI_COMM_WORLD);
+        MPI_BinomialCast(&n,1,MPI_INT,0,MPI_COMM_WORLD);
         if (n == 0)
             break;
                 
         count = 0;  
 
-        for (i = rank; i <= n; i+=numprocs) {
+        for (i = rank+1; i <= n; i+=numprocs) {
             // Get the random numbers between 0 and 1
             x = ((double) rand()) / ((double) RAND_MAX);
             y = ((double) rand()) / ((double) RAND_MAX);
